9108d14ac8f44f61262442ae98e34d623d50e260
Sarah Pitts <mccallsmedia@gmail.com>
2013-10-31 10:12:23 -0500

    Implement validations on person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a last name' do
+    person = Person.new(first_name: 'Bob', last_name: nil)
+    expect(person).not_to be_valid
+  end
 end
+
+class Person < ActiveRecord::Base
+  attr_accessible :first_name, :last_name
+
+  validates :first_name, :last_name, presence: true
+end
+
