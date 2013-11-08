566daf570964fdb291217de6ab0a7d4be44aae44
Sarah Pitts <mccallsmedia@gmail.com>
2013-11-08 10:16:09 -0600

    Adds Delete option to Phone Numbers

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -159,10 +159,10 @@ describe PhoneNumbersController do
       }.to change(PhoneNumber, :count).by(-1)
     end
 
-    it "redirects to the phone_numbers list" do
+    it "redirects to the phone_number person" do
       phone_number = PhoneNumber.create! valid_attributes
       delete :destroy, {:id => phone_number.to_param}, valid_session
-      response.should redirect_to(phone_numbers_url)
+      response.should redirect_to(phone_number.person)
     end
   end
 
