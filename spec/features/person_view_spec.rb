a7942ef40132a944b0acc780a80efb38e0621825
Sarah Pitts <mccallsmedia@gmail.com>
2013-11-21 09:29:04 -0600

    attempts to add Edit email address

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -76,9 +76,21 @@ let(:person) { Person.create(first_name: 'Jane', last_name: 'Doe') }
     it 'adds a new email_address' do
       page.click_link("Add email address")
       page.fill_in('Address', with: "addedemail@example.com")
-      page.fill_in('email_address_person_id', with: "1")
+      /page.fill_in('email_address_person_id', with: "1")/
       page.click_button("Create Email address")
       expect(current_path).to eq(person_path(person))
       expect(page).to have_content('addedemail@example.com')
+    end
+
+    it 'edits an email address' do
+      email = person.email_addresses.first
+      old_email_address = email.address
+      
+      page.click_link("Edit")
+      page.fill_in('address', with: 'new@example.com')
+      page.click_button("Update Email address")
+      expect(current_path).to eq(person_path(person))
+      expect(page).to have_content('new@example.com')
+      expect(page).to_not have_content(old_email_address)
     end
 end
