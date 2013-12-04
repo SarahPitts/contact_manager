e86c6ee5f35ac274da608d2c756c0441561f0530
Sarah Pitts <mccallsmedia@gmail.com>
2013-12-04 16:39:39 -0600

    corrects failures for person_view_spec

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
   attr_accessible :first_name, :last_name
   has_many :phone_numbers,  as: :contact
-  has_many :email_addresses
+  has_many :email_addresses, as: :contact
 
   validates :first_name, :last_name, presence: true
 end
